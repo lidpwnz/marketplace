76115ee0484867545b958fe3ab475f3e1dcbc0be
davidlee <lidpwnz@gmail.com>
2021-08-07 16:03:49 +0600

    #6 описал роуты для марок

--- config_files/urls.py
+++ config_files/urls.py
@@ -1,4 +1,5 @@
 from controllers.cars_controller import CarsController
+from controllers.makes_controller import MakesController
 from http_fw.router import Router
 
 router = Router()
@@ -10,4 +11,13 @@ router.get('/advertisements/detail', CarsController, 'detail')
 router.post('/advertisements/delete', CarsController, 'delete')
 router.get('/advertisements/update', CarsController, 'update_get')
 router.post('/advertisements/update', CarsController, 'update_post')
+
+router.get('/makes', MakesController, 'list')
+router.get('/makes/add', MakesController, 'new')
+router.post('/makes/add', MakesController, 'create')
+router.get('/makes/detail', MakesController, 'detail')
+router.get('/makes/update', MakesController, 'update_get')
+router.post('/makes/update', MakesController, 'update_post')
+router.post('/makes/delete', MakesController, 'delete')
+
 
